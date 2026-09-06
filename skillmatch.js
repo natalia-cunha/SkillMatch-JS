4669fd0eaea300acba5c6bd5f6327365d119a44f
Natália Cunha <ndscunha@gmail.com>
2026-09-06 20:28:05 -0300

    feat: cria candidatos e vagas

---
+++ skillmatch.js
@@ -0,0 +1,44 @@
+const candidatos = [
+    {
+        nome: "Natália de Souza Cunha",
+        areaInteresse: "Front-end",
+        habilidades: ["HTML", "CSS", "JavaScript"],
+        experiencia: 0
+    },
+    {
+        nome: "Ana Beatriz Silva",
+        areaInteresse: "Front-end",
+        habilidades: ["HTML", "CSS", "JavaScript"],
+        experiencia: 1
+    },
+    {
+        nome: "Lucas Oliveira",
+        areaInteresse: "Front-end",
+        habilidades: ["HTML", "CSS", "JavaScript", "Git"],
+        experiencia: 2
+    },
+    {
+        nome: "Mariana Santos",
+        areaInteresse: "Front-end",
+        habilidades: ["HTML", "CSS", "JavaScript", "React"],
+        experiencia: 1
+    }
+];
+
+const vagas = [
+    {
+        empresa: "Tech Solutions",
+        cargo: "Desenvolvedor Front-end Júnior",
+        requisitos: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        empresa: "CodeLab",
+        cargo: "Desenvolvedor Front-end Júnior",
+        requisitos: ["HTML", "CSS", "JavaScript", "Git"]
+    },
+    {
+        empresa: "WebStart",
+        cargo: "Desenvolvedor Front-end Júnior",
+        requisitos: ["HTML", "CSS", "JavaScript", "React"]
+    }
+];
